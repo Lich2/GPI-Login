881b86d4ee59af8286f5d02db2f6161a6aa1a8bb
Jorge <nagole4@hotmail.com>
2016-09-30 18:12:16 -0300

    creacion de nueva clase

---
+++ src/main/java/pico/pal/que/Sarita.java
@@ -0,0 +1,26 @@
+package pico.pal.que;
+
+public class Sarita {
+
+	private String mensaje;
+	
+	
+		
+	public String getMensaje() {
+		return mensaje;
+	}
+
+
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
